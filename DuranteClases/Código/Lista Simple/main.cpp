aa85aa340eddd551f22b00ef1733e609d5c80b1f
Hector <hectorleivagamboa0741@gmail.com>
2024-08-11 19:25:25 -0600

    Arreglo de main.cpp

--- DuranteClases/Código/Lista Simple/main.cpp
+++ DuranteClases/Código/Lista Simple/main.cpp
@@ -9,66 +9,61 @@ int main(){
 	// PRUEBAS
 	
 	ListaSimple * L1 = new ListaSimple();
-	L1->insertarAlInicio(1);
-	L1->insertarAlInicio(2);
-	L1->insertarAlInicio(3);
-	L1->insertarAlInicio(4);
+	Estudiante E1("Juan", "Perez", "Ingeniería", "01/01/2000");
+    Estudiante E2("Maria", "Gomez", "Medicina", "02/02/1999");
+    Estudiante E3("Luis", "Ramirez", "Derecho", "03/03/1998");
+    Estudiante E4("Ana", "Martinez", "Arquitectura", "04/04/1997");
+
+	L1->insertarAlInicio(E1);
+    L1->insertarAlInicio(E2);
+    L1->insertarAlInicio(E3);
 
 	// 4 3 2 1
 
 	L1->imprimir();
 
-	L1->insertarAlFinal(5);
+	L1->insertarAlFinal(E4);
 
 	// 4 3 2 1 5
 	L1->imprimir();
 
-	Nodo * e1 = L1->eliminar(5);
+	Nodo * e1 = L1->eliminar(E1);
 
 
-	(e1 != NULL) ? std::cout << "Se elimino de la lista simple el " << e1->dato << std::endl : std::cout << "No lo encontro" << std::endl;
+	(e1 != NULL) ? std::cout << "Se elimino de la lista simple a " << e1->dato.nombre << std::endl : std::cout << "No lo encontro" << std::endl;
 
 	delete e1;
 
 	// 4 3 2 1
 	L1->imprimir();
 
-	Nodo * f1 = L1->eliminar(4);
+	Nodo * f1 = L1->eliminar(E3);
 
-	(f1 != NULL) ? std::cout << "Se elimino de la lista simple el " << f1->dato << std::endl : std::cout << "No lo encontro" << std::endl;
+	(f1 != NULL) ? std::cout << "Se elimino de la lista simple a " << f1->dato.nombre << std::endl : std::cout << "No lo encontro" << std::endl;
 	
 	delete f1;
 	
 	// 3 2 1
 	L1->imprimir();
 	
-	Nodo * g1 = L1->eliminar(2);
+	Nodo * g1 = L1->eliminar(E2);
 	
-	(g1 != NULL) ? std::cout << "Se elimino de la lista simple el " << g1->dato << std::endl : std::cout << "No lo encontro" << std::endl;
+	(g1 != NULL) ? std::cout << "Se elimino de la lista simple a " << g1->dato.nombre << std::endl : std::cout << "No lo encontro" << std::endl;
 	
 	delete g1;
 	
 	// 3 1
 	L1->imprimir();
 	
-	Nodo * h1 = L1->eliminar(1);
+	Nodo * h1 = L1->eliminar(E4);
 	
-	(h1 != NULL) ? std::cout << "Se elimino de la lista simple el " << h1->dato << std::endl : std::cout << "No lo encontro" << std::endl;
+	(h1 != NULL) ? std::cout << "Se elimino de la lista simple a " << h1->dato.nombre << std::endl : std::cout << "No lo encontro" << std::endl;
 
 	delete h1;
 
 	// 3
 	L1->imprimir();
 
-	Nodo * i1 = L1->eliminar(3);
-
-	(i1 != NULL) ? std::cout << "Se elimino de la lista simple el " << i1->dato << std::endl : std::cout << "No lo encontro" << std::endl;
-
-	delete i1;
-
-	// 
-	L1->imprimir();
-	
 	return 0;
 }
 
